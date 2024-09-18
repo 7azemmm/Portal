c62663422f0391f96b3784cc352bc3ec2950a21f
Hazem Mohamed <hazemm7622@gmail.com>
2024-09-18 11:05:55 +0300

    finalize attendance feature

--- WebApplication1/Controllers/AttendanceController.cs
+++ WebApplication1/Controllers/AttendanceController.cs
@@ -11,16 +11,40 @@ public class AttendanceController : Controller
    
     ApplicationContext context = new ApplicationContext();
 
-  public IActionResult TakeAttendance(Student_Attendance stud)
+  public IActionResult TakeAttendance(List<Student_Attendance> stud)
 {
-    context.Student_Attendance.Add(stud);
+    
+ foreach (var student in stud)
+    {
+        var studentAttendance = new Student_Attendance
+        {
+            
+            Name = student.Name,
+            Attendance_State = student.Attendance_State,
+            studentId = student.studentId 
+        };
+
+        context.Student_Attendance.Add(studentAttendance);
+    }
+
     context.SaveChanges();
-    return RedirectToAction("Index", "Home");
+
+    Console.WriteLine(stud);
+    Console.WriteLine(stud.Count);
+    return Content("attendance taken");
 }
 
 public IActionResult Get_Student_Attendance(int ID)
 {
-    var studAttendance = context.Student_Attendance.Where(x => x.studentId == ID).ToList();
+    var studAttendance = context.Student_Attendance
+    .Where(x => x.studentId == ID).ToList();
+
+    var student = context.Students.Find(ID);
+    ViewBag.student = student ; 
+
+    var groups = context.Groups.Find(ViewBag.student.groupId);
+
+    ViewBag.group = groups;
     
     if (!studAttendance.Any())
     {
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe226cc21130f8b6cf425c6d1afdcdb7f9d5175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7506e1e09bf42b3cc3f7bcacf43237e00dd3bb12")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
